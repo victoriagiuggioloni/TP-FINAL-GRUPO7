8cae3d65310ee26cedfb2c3445290b7f588dccad
NHermann06 <nhermann@udesa.edu.ar>
2025-11-06 12:39:30 -0300

    cambios

--- visualizacion_juego.py
+++ visualizacion_juego.py
@@ -1,4 +1,3 @@
-import os
 import pygame
 
 #Inicializamos el Juego
@@ -11,18 +10,9 @@ screen = pygame.display.set_mode((1024,1024))
 #cambiamos nombre del juego
 pygame.display.set_caption("Angry Flappy Bird")
 
-#obtenemos ruta del archivo del icono
-base_path = os.path.dirname(__file__)
-icon_path = os.path.join(base_path, "bird.png.png")
-
 #creamos un icono para nuestro juego
-icon = pygame.image.load(icon_path)
-#cambiamos tamano de icono
-icono = pygame.transform.scale(icon, (32,32))
-pygame.display.set_icon(icono)
-
-
-
+icon = pygame.image.load('icono.png')
+pygame.display.set_icon(icon)
 
 #Generamos el loop del juego
 running = True
